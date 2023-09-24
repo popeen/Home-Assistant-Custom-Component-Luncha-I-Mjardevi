c719acf7e7a2c31db849fcf25194080d9a4e4036
Patrik Johansson <patrik@ptjwebben.se>
2023-09-24 20:48:38 +0200

    Added API key validation to ConfigFlow

--- custom_components/lunchaimjardevi/config_flow.py
+++ custom_components/lunchaimjardevi/config_flow.py
@@ -3,12 +3,9 @@ from . import common
 from typing import Any
 
 from homeassistant import config_entries, exceptions
-from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-import asyncio
 import voluptuous as vol
-
 
 DATA_SCHEMA = vol.Schema(
     {
@@ -16,6 +13,18 @@ DATA_SCHEMA = vol.Schema(
     }
 )
 
+async def validate_input(hass: HomeAssistant, data: dict) -> dict[str, Any]:
+    """Validate the user input."""
+    session = async_get_clientsession(hass)  
+    key = data[common.CONF_APIKEY]      
+    try:
+        url = "https://lunchaimjardevi.com/api/v4/validateKey?key=" + key
+        async with session.get(url) as resp:
+            json = await resp.json()
+            if False == json['valid']:
+                raise Exception("the key is invalid")
+    except Exception:
+        raise InvalidAPIKey
 
 class ConfigFlow(config_entries.ConfigFlow, domain=common.DOMAIN):
 
@@ -27,7 +36,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=common.DOMAIN):
         errors = {}
         if user_input is not None:
             try:
+                await validate_input(self.hass, user_input)
                 return self.async_create_entry(title="Luncha i Mjärdevi", data=user_input)
+            except InvalidAPIKey:
+                errors["base"] = "invalid_api_key"
             except Exception:  # pylint: disable=broad-except
                 common._LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
@@ -36,3 +48,5 @@ class ConfigFlow(config_entries.ConfigFlow, domain=common.DOMAIN):
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
         
+class InvalidAPIKey(exceptions.HomeAssistantError):
+    """Error to indicate there is no data available for the ID."""
